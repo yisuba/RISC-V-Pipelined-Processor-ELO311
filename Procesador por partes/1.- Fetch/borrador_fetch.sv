841f98067ac323df223da0fbd8a99c4ccb5bc411
YisuhCraist <cristobal.fuentesdelcanto@gmail.com>
2025-01-20 13:51:54 -0300

    Avances 20/01
    Etapa de Fetch parcialmente terminada.
    Falta realizar testbenchs para probar su funcionamiento
    Falta conectar etapa de Fetch en procesador completo

--- Procesador por partes/1.- Fetch/borrador_fetch.sv
+++ Procesador por partes/1.- Fetch/borrador_fetch.sv
@@ -46,33 +46,40 @@ module Instruction_Fetch_Stage(
 	logic [31:0] InstrF;
 
 	Instruction_Memory InstMem(								// Instruction Memory *Solo carcasa del módulo
-	.rst(rst),
 	.A(PCF_postff),
-	.RD(InstrF)
+	.RD(InstrF)												//ReadData
 	);
 
 endmodule
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-module Instruction_Memory(									// Instruction Memory *Solo carcasa del módulo
-	input logic [31:0] A,
-	output logic [31:0] RD
-	);
+// Instruction Memory 
+module Instruction_Memory #(parameter WordQuantity = 256)(	// Instruction Memory de 1 KiloByte de memoria (256*4)
+	input logic [31:0] address,
+	output logic [31:0] instruction					
 
+	logic [31:0] memory [WordQuantity-1:0];					// memory se convierte en un array de WordQuantity, lo que entrega la cantidad de memoria máxima (en [bytes]) dado por WordQuantity * 4[byte/word]
+
+    initial begin											// Inicialización de un programa (asignación instrucciones en memoria)
+	
+        memory[0]  = 32'h00000093; // ADDI x1, x0, 0 (verificar todos)	//dirección 0x0000_0000 -> ... 0000_00|00
+        memory[1]  = 32'h00100113; // ADDI x2, x0, 1					//dirección 0x0000_0004 -> ... 0000_01|00
+        memory[2]  = 32'h00208193; // ADDI x3, x1, 2					//dirección 0x0000_0008 -> ... 0000_10|00
+        memory[3]  = 32'h00310213; // ADDI x4, x2, 3					//dirección 0x0000_000C -> ... 0000_11|00
+        memory[4]  = 32'h00418293; // ADDI x5, x3, 4					//dirección 0x0000_0010 -> ... 0001_00|00
+        memory[5]  = 32'h00520313; // ADDI x6, x4, 5					//dirección 0x0000_0014 -> ... 0001_01|00
+		
+        for (int i = 6; i < 256; i++) begin
+            memory[i] = 32'h00000000; 						// El resto del programa (resto de la memoria) se rellenará con NOPs
+        end
+    end	
+
+    always_comb begin
+        instruction = memory[address[9:2]]; 				// address[9:2] entrega el valor que existe en esos 8 bits como un decimal para castear una posición del array memory
+    end
+	
 endmodule
 
-	logic [31:0] InstrD, PCD, PCPlus4D;
-	always_ff @(posedge clk or posedge rst) begin			// FF de salida de la Instruction_Fetch
-		if(rst) begin
-			InstrD <= 32'b0;
-			PCD <= 32'b0;
-			PCPlus4D <= 32'b0;
-			end
-		else if(!StallD) begin
-			InstrD <= InstrF;
-			PCD <= PCF_postff;
-			PCPlus4D <= PCPlus4F;
-			end
-	end
+
 			
--- Procesador por partes/RISCV_Processor.sv
+++ Procesador por partes/RISCV_Processor.sv
@@ -9,8 +9,23 @@ Instruction_Fetch IF(
 
 );	
 
-   input logic clk, rst,
+	input logic clk, rst,
     input logic PCSrcE,
     input logic [31:0] PCTargetE,
     inout logic [31:0] PCPlus4F,
     output logic [31:0] PCF_postff
+	
+
+	logic [31:0] InstrD, PCD, PCPlus4D;
+	always_ff @(posedge clk or posedge rst) begin			// FF de salida de la Instruction_Fetch
+		if(rst) begin
+			InstrD <= 32'b0;
+			PCD <= 32'b0;
+			PCPlus4D <= 32'b0;
+			end
+		else if(!StallD) begin
+			InstrD <= InstrF;
+			PCD <= PCF_postff;
+			PCPlus4D <= PCPlus4F;
+			end
+	end
