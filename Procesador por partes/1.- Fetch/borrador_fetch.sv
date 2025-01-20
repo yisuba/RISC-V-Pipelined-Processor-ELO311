98db9e67b2e05cc221ae25c5545a29bf2bd32b63
YisuhCraist <cristobal.fuentesdelcanto@gmail.com>
2025-01-20 15:44:17 -0300

    Instruction Fetch v.091
    Actualización bajo revisión.
    Aún falta el testbench respectivo y opción de cargar instrucciones mediante un archivo externo.

--- Procesador por partes/1.- Fetch/borrador_fetch.sv
+++ Procesador por partes/1.- Fetch/borrador_fetch.sv
@@ -9,7 +9,7 @@
 
 module Instruction_Fetch_Stage(
     input logic clk, rst, 
-	input logic PCSrcE, StallF
+	input logic PCSrcE, StallF,
     input logic [31:0] PCTargetE,
     inout logic [31:0] PCPlus4F,
     output logic [31:0] PCF_postff, InstrF
@@ -30,7 +30,7 @@ module Instruction_Fetch_Stage(
             1'b0: PCF_preff = PCPlus4F;
             1'b1: PCF_preff = PCTargetE;
         endcase 
-    //También puede utilizarse "assign PCF_preff = PCSrcE ? PCTargetE : PCPlus4F;"
+    //También puede utilizarse "assign PCF_preff = PCSrcE ? PCTargetE : PCPlus4F;" para simplificar el always_comb
     end
 
     always_ff @(posedge clk or posedge rst) begin  			// FF del Program Counter
@@ -42,11 +42,9 @@ module Instruction_Fetch_Stage(
 
 	assign PCPlus4F = PCF_postff + 32'd4;                	// Adder para el PC+4
 
-	logic [31:0] InstrF;
-
 	Instruction_Memory InstrMem(							// Instruction Memory *Solo carcasa del módulo
-	.A(PCF_postff),
-	.RD(InstrF)												// ReadData
+	.address(PCF_postff),
+	.instruction(InstrF)						
 	);
 
 endmodule
@@ -55,12 +53,12 @@ endmodule
 
 // Instruction Memory 
 module Instruction_Memory #(parameter WordQuantity = 256, parameter BitSize = 8)(	// Instruction Memory de 1 KiloByte de memoria (256*4)
-	input logic [31:0] address,
-	output logic [31:0] instruction					
-
+	input logic [31:0] address,				                // Address
+	output logic [31:0] instruction				     		// ReadData	/ instruction		
+);
 	logic [31:0] memory [WordQuantity-1:0];					// memory se convierte en un array de WordQuantity, lo que entrega la cantidad de memoria máxima (en [bytes]) dado por WordQuantity * 4[byte/word]
 
-    initial begin											// Inicialización de un programa (asignación instrucciones en memoria)
+    initial begin											// Inicialización de un programa (asignación instrucciones en memoria) ***REVISAR PARA 	CARGAR INSTRUCCIONES DESDE UN ARCHIVO
 	
         memory[0]  = 32'h00000093; // ADDI x1, x0, 0 (verificar todos)	//dirección 0x0000_0000 -> ... 0000_00|00
         memory[1]  = 32'h00100113; // ADDI x2, x0, 1					//dirección 0x0000_0004 -> ... 0000_01|00
